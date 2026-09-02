83a8c0efac1d890feab5bd75c63668e501d51f2c
aslan-89-cpu <aslan89aslan1@gmail.com>
2026-09-02 16:31:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -65,4 +65,6 @@ app.post('/api/verify-channel', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(Server running safely on port ${PORT}));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(Server running safely on port ${PORT});
+});
